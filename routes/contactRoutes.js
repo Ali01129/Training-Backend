8fdc534ce5de5f51f9d86b46caf8950b3da197bc
Ali01129 <chaudharyalinawazz@gmail.com>
2025-07-03 19:04:42 +0500

    done crud operation with contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,8 @@
 const express=require("express");
 const router=express.Router();
-const {getContact,createContact} = require('../controllers/contactController');
+const {getContact,createContact,getContactbyID,updateContact,deleteContact} = require('../controllers/contactController');
 
 router.route('/').get(getContact).post(createContact);
+router.route('/:id').get(getContactbyID).put(updateContact).delete(deleteContact);
 
 module.exports=router;
